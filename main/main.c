19e9718b7b90be30d4c6f412b429a51251d7810e
JuanRevilla <jrevillatienza@hotmail.com>
2023-05-09 12:09:51 +0200

    pidversion

--- main/main.c
+++ main/main.c
@@ -1,6 +1,127 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/i2c.h"
+#include "driver/mcpwm_prelude.h"
+#include "driver/mcpwm_timer.h"
+#include "pid_ctrl.h"
 
-void app_main(void)
+
+const static char *TAG = "i2c";
+#define SERVO_MIN_PULSEWIDTH_US 500  // Minimum pulse width in microsecond
+#define SERVO_MAX_PULSEWIDTH_US 2500  // Maximum pulse width in microsecond
+#define SERVO_MIN_DEGREE        -90   // Minimum angle
+#define SERVO_MAX_DEGREE        90    // Maximum angle
+
+#define SERVO_PULSE_GPIO             0        // GPIO connects to the PWM signal line
+#define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
+#define SERVO_TIMEBASE_PERIOD        20000    // 20000 ticks, 20ms
+
+#define ESP_SLAVE_ADDR	0x11
+#define DELAY_MS		100
+#define I2C_SLAVE_SDA_IO 21
+#define I2C_SLAVE_SCL_IO 22
+static inline uint32_t example_angle_to_compare(int angle)
 {
+    return (angle - SERVO_MIN_DEGREE) * (SERVO_MAX_PULSEWIDTH_US - SERVO_MIN_PULSEWIDTH_US) / (SERVO_MAX_DEGREE - SERVO_MIN_DEGREE) + SERVO_MIN_PULSEWIDTH_US;
+}
 
+void app_main() {
+    int error=0;
+    float errorPID=0;
+    int angle=0;
+    float convFact=0.1;
+	uint8_t rx_data[5];
+        ESP_LOGI(TAG, "Create timer and operator");
+    mcpwm_timer_handle_t timer = NULL;
+    mcpwm_timer_config_t timer_config = {
+        .group_id = 0,
+        .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
+        .resolution_hz = SERVO_TIMEBASE_RESOLUTION_HZ,
+        .period_ticks = SERVO_TIMEBASE_PERIOD,
+        .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &timer));
+
+    mcpwm_oper_handle_t oper = NULL;
+    mcpwm_operator_config_t operator_config = {
+        .group_id = 0, // operator must be in the same group to the timer
+    };
+    ESP_ERROR_CHECK(mcpwm_new_operator(&operator_config, &oper));
+
+    ESP_LOGI(TAG, "Connect timer and operator");
+    ESP_ERROR_CHECK(mcpwm_operator_connect_timer(oper, timer));
+
+    ESP_LOGI(TAG, "Create comparator and generator from the operator");
+    mcpwm_cmpr_handle_t comparator = NULL;
+    mcpwm_comparator_config_t comparator_config = {
+        .flags.update_cmp_on_tez = true,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_comparator(oper, &comparator_config, &comparator));
+
+    mcpwm_gen_handle_t generator = NULL;
+    mcpwm_generator_config_t generator_config = {
+        .gen_gpio_num = SERVO_PULSE_GPIO,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_generator(oper, &generator_config, &generator));
+
+    // set the initial compare value, so that the servo will spin to the center position
+    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, example_angle_to_compare(0)));
+
+    ESP_LOGI(TAG, "Set generator action on timer and compare event");
+    // go high on counter empty
+    ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_timer_event(generator,
+                    MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH)));
+    // go low on compare threshold
+    ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_compare_event(generator,
+                    MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparator, MCPWM_GEN_ACTION_LOW)));
+
+    ESP_LOGI(TAG, "Enable and start timer");
+    ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
+    ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP));
+
+    i2c_config_t conf_slave = {
+        .sda_io_num = I2C_SLAVE_SDA_IO,            // select SDA GPIO specific to your project
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_SLAVE_SCL_IO,            // select SCL GPIO specific to your project
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .mode = I2C_MODE_SLAVE,
+        .slave.addr_10bit_en = 0,
+        .slave.slave_addr = ESP_SLAVE_ADDR,        // slave address of your project
+    };
+	i2c_param_config(I2C_NUM_0, &conf_slave);
+	ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_SLAVE, 0, 0, 0));
+    //PID Configuration
+        pid_ctrl_parameter_t pid_runtime_param = {
+        .kp = 0.6,
+        .ki = 0.4,
+        .kd = 0.2,
+        .cal_type = PID_CAL_TYPE_INCREMENTAL,
+        .max_output   = 100,
+        .min_output   = -100,
+        .max_integral = 1000,
+        .min_integral = -1000,
+    };
+    pid_ctrl_block_handle_t pid_ctrl = NULL;
+    pid_ctrl_config_t pid_config = {
+        .init_param = pid_runtime_param,
+    };
+    ESP_ERROR_CHECK(pid_new_control_block(&pid_config, &pid_ctrl));
+	while (1) {
+		error=i2c_slave_read_buffer(I2C_NUM_0, rx_data, 5, 1000 / portTICK_PERIOD_MS);
+        pid_compute(pid_ctrl,(float)error,&errorPID);
+        angle=(int)(errorPID*convFact);
+        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, example_angle_to_compare(angle)));
+		ESP_LOG_BUFFER_HEX(TAG, rx_data, 5);
+        ESP_LOGI(TAG,"%i",angle);
+		vTaskDelay(DELAY_MS/portTICK_PERIOD_MS);
+	}
 }
